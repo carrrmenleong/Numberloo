59ce838015ed27d95ef096c1fdf8da696b692b68
Carmen <carmen020824@gmail.com>
2022-07-19 14:16:43 +0800

    heroku deployment changes

--- app/__init__.py
+++ app/__init__.py
@@ -22,6 +22,24 @@ def create_app(config_name):
     app.config.from_pyfile('config.py')
     db.init_app(app)
 
+    if app.config['LOG_TO_STDOUT']:
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(logging.INFO)
+            app.logger.addHandler(stream_handler)
+    else:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/numberloo.log',
+                                            maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s '
+            '[in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Numberloo startup')
+
     return app
 
 from app import routes, models
